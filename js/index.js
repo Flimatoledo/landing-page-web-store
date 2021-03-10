147892c14f8903f018fd43ca7403254a6220f538
Felipe Toledo <felip_elima@hotmail.com>
2021-03-09 23:31:52 -0300

    first commit of an online store layout

---
+++ js/index.js
@@ -0,0 +1,24 @@
+(() => {
+  let time = 3500,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img")
+  max = images.length;
+
+  const nextImg = () => {
+
+    images[currentImageIndex].classList.remove('selected');
+    currentImageIndex += 1;
+
+    if (currentImageIndex >= max)
+      currentImageIndex = 0;
+
+    images[currentImageIndex].classList.add('selected');
+  }
+
+  window.addEventListener('load',() => {
+    setInterval(() => {
+      nextImg();
+
+    }, time)
+  })
+})();
